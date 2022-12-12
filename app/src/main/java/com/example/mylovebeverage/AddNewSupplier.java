6cbdf9747449433dfeac323b2701a6569d33cc98
Withyou11 <tranquangtin1111@gmail.com>
2022-12-12 12:32:03 +0700

    manage supplier

---
+++ app/src/main/java/com/example/mylovebeverage/AddNewSupplier.java
@@ -0,0 +1,105 @@
+package com.example.mylovebeverage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.mylovebeverage.Data.Connecting_MSSQL;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class AddNewSupplier extends AppCompatActivity {
+    ImageView arrback;
+    EditText edt1;
+    EditText edt2;
+    EditText edt3;
+    EditText edt4;
+    EditText edt5;
+
+    Button btn1;
+    Button btn2;
+    Connecting_MSSQL connecting_mssql;
+    private static Connection connection_addSupplier = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_supplier);
+
+        connecting_mssql = new Connecting_MSSQL(connection_addSupplier);
+        connection_addSupplier = connecting_mssql.Connecting();
+        arrback = findViewById(R.id.arrow_back);
+        arrback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        edt1 = (EditText) findViewById(R.id.supplier_addSupplierName);
+        edt2 = (EditText) findViewById(R.id.supplier_addSupplierLogo);
+        edt3 = (EditText) findViewById(R.id.supplier_addSupplierAddress);
+        edt4 = (EditText) findViewById(R.id.supplier_addSupplierEmail);
+        edt5 = (EditText) findViewById(R.id.supplier_addSupplierPhone);
+
+        btn1 = (Button) findViewById(R.id.addNewSupplier);
+        btn2 = (Button) findViewById(R.id.cancelNewSupplier);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s1 = edt1.getText().toString();
+                String s2 = edt2.getText().toString();
+                String s3 = edt3.getText().toString();
+                String s4 = edt4.getText().toString();
+                String s5 = edt5.getText().toString();
+
+                int i = 1;
+                if (s1.equals("") || s2.equals("") || s3.equals("") || s4.equals("") || s5.equals("")) {
+                    Toast.makeText(getApplicationContext(), "Please fill all information", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (connection_addSupplier != null) {
+                        try {
+                            Statement statement = connection_addSupplier.createStatement();
+                            ResultSet resultSet = statement.executeQuery("select * from [dbo].[Supplier];");
+                            while (resultSet.next()) {
+                                i += 1;
+                            }
+
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Connect to Supplier makes error.", Toast.LENGTH_SHORT).show();
+                    }
+
+                    String Supplier_ID = "SP" + i;
+                    try {
+                        Statement statement2 = connection_addSupplier.createStatement();
+                        ResultSet resultSet = statement2.executeQuery("INSERT INTO [dbo].[SUPPLIER] (Supplier_ID, Name_of_supplier, Address, Email, PhoneNumber, Status, Logo, TotalBill, TotalMoney)\n" +
+                                "VALUES ('"+Supplier_ID+"', '"+s1+"', '"+s3+"', '"+s4+"', '"+s5+"', 'active', '"+s2+"', '0', '0');");
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    Intent intent = new Intent();
+                    intent.putExtra("editTextValue", "value_here");
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/mylovebeverage/ManageSupplier.java
@@ -0,0 +1,107 @@
+package com.example.mylovebeverage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.mylovebeverage.Adapters.ProductAdapter;
+import com.example.mylovebeverage.Adapters.SupplierAdapter;
+import com.example.mylovebeverage.Data.Connecting_MSSQL;
+import com.example.mylovebeverage.Models.Product;
+import com.example.mylovebeverage.Models.Supplier;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class ManageSupplier extends AppCompatActivity {
+
+    private ListView listView;
+    Connecting_MSSQL connecting_mssql;
+    private static Connection connection_supplier = null;
+    ImageView arrback;
+
+    Supplier supplier;
+    public static ArrayList<Supplier> suppliersList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_supplier);
+        connecting_mssql = new Connecting_MSSQL(connection_supplier);
+        connection_supplier = connecting_mssql.Connecting();
+        getAllSuppliers();
+        setUpList();
+        setUpOnClickListener();
+        arrback = (ImageView) findViewById(R.id.arrow_back);
+        arrback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        FloatingActionButton addSuppliertBtn = findViewById(R.id.addSupplier);
+        addSuppliertBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AddNewSupplier.class);
+//                startActivity(intent);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+    protected void getAllSuppliers(){
+        if (connection_supplier!=null)
+        {
+            try {
+                Statement statement = connection_supplier.createStatement();
+                ResultSet resultSet = statement.executeQuery("select * from [dbo].[SUPPLIER] where Status = 'active';");
+                suppliersList.clear();
+                while (resultSet.next()) {
+                    supplier = new Supplier(resultSet.getString(1).trim(),resultSet.getString(2).trim(), resultSet.getString(3).trim(), resultSet.getString(4).trim(), resultSet.getString(5).trim(), resultSet.getString(7).trim(),resultSet.getInt(8), resultSet.getInt(9));
+                    suppliersList.add(supplier);
+                }
+
+            }catch (SQLException e)
+            {
+                e.printStackTrace();
+            }
+        }else
+        {
+            Toast.makeText(getApplicationContext(), "Connect to ProductDB makes error." , Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void setUpList() {
+        listView = (ListView) findViewById(R.id.suppliersListView);
+        SupplierAdapter adapter = new SupplierAdapter(getApplicationContext(), 0, suppliersList);
+        listView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+    private void setUpOnClickListener() {
+
+        listView.setOnItemClickListener((adapterView, view, i, l) -> {
+            Supplier selectSupplier = (Supplier) (listView.getItemAtPosition(i));
+            Intent showDetail = new Intent(getApplicationContext(), SupplierDetail.class);
+            showDetail.putExtra("id",selectSupplier.getSupplier_ID());
+            startActivityForResult(showDetail, 1);
+        });
+
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if(resultCode == RESULT_OK) {
+                getAllSuppliers();
+                setUpList();
+            }
+        }
+    }
+}
