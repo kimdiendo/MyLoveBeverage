4f293020f874977eaaf21bd3efbef3d7e11e5b06
kimdiendo <huynhdien520@gmail.com>
2023-02-05 22:14:51 +0700

    bổ sung hotline và aboutus

--- app/src/main/java/com/example/mylovebeverage/Manager.java
+++ app/src/main/java/com/example/mylovebeverage/Manager.java
@@ -3,6 +3,8 @@ package com.example.mylovebeverage;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.NavDestination;
@@ -11,14 +13,18 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.mylovebeverage.Data.Connecting_MSSQL;
 import com.example.mylovebeverage.databinding.ActivityManagerBinding;
 import com.google.android.material.navigation.NavigationView;
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -28,7 +34,9 @@ public class Manager extends AppCompatActivity {
     private ActivityManagerBinding binding;
     private static Connection connection_manager;
     private String username ="";
-    private String password ="";
+    private String password = "";
+    private static final int PERMISSION_REQUEST_CODE_CALL = 123;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +47,42 @@ public class Manager extends AppCompatActivity {
         username = intent.getStringExtra("username");
         password = intent.getStringExtra("password");
     }
+
+    private void requestCallPermission() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.CALL_PHONE)) {
+                //provide explannation to user that s' why they must provide permission
+            }
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CALL_PHONE}, PERMISSION_REQUEST_CODE_CALL);
+        } else {
+            // Permission has already been granted
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE_CALL) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCalling();
+            } else {
+                Toast.makeText(this, "Calling Permission has denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void openCalling() {
+        if (ContextCompat.checkSelfPermission(Manager.this, android.Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+            Intent intent_call = new Intent();
+            intent_call.setAction(Intent.ACTION_CALL);
+            intent_call.setData(Uri.parse("tel: " + "0964376592"));
+            startActivity(intent_call);
+        } else {
+            // Permission has not been granted, request it
+            requestCallPermission();
+        }
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -48,6 +92,7 @@ public class Manager extends AppCompatActivity {
     protected void onRestart() {
         super.onRestart();
     }
+
     @Override
     protected void onPause() {
         super.onPause();
@@ -104,17 +149,31 @@ public class Manager extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 if (item.getTitle().toString().trim().equals("Profile")) {
                     navController.navigate(R.id.profile, bundle);
-                }else if (item.getTitle().toString().trim().equals("Home"))
-                {
+                }else if (item.getTitle().toString().trim().equals("Home")) {
                     navController.navigate(R.id.home);
-                }else if (item.getTitle().toString().trim().equals("About Us"))
-                {
-                    navController.navigate(R.id.aboutus);
-                }else if (item.getTitle().toString().trim().equals("Statistic"))
-                {
+                } else if (item.getTitle().toString().trim().equals("Hotline")) {
+                    Dialog dialog = new Dialog(Manager.this);
+                    dialog.setContentView(R.layout.activity_hotline);
+                    Button btnYes = dialog.findViewById(R.id.btnCustomDialogYes);
+                    Button btnNo = dialog.findViewById(R.id.btnCustomDialogNo);
+                    dialog.setCanceledOnTouchOutside(false);
+                    dialog.show();
+                    btnNo.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            dialog.dismiss();
+                        }
+                    });
+                    btnYes.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            openCalling();
+                            dialog.dismiss();
+                        }
+                    });
+                }else if (item.getTitle().toString().trim().equals("Statistic")) {
                     navController.navigate(R.id.statistic);
-                }
-                else if (item.getTitle().toString().trim().equals("Log Out")) {
+                } else if (item.getTitle().toString().trim().equals("Log Out")) {
                     Dialog dialog = new Dialog(Manager.this);
                     dialog.setContentView(R.layout.activity_log_out);
                     Button btnYes = dialog.findViewById(R.id.btnCustomDialogYes);
@@ -134,16 +193,20 @@ public class Manager extends AppCompatActivity {
                                 Statement statement = connection_manager.createStatement();
                                 statement.execute("UPDATE ACCOUNT\n" +
                                         "SET Status ='inactive'\n" +
-                                        "WHERE Account_name ="+"'"+username+"'");
+                                        "WHERE Account_name =" + "'" + username + "'");
                                 dialog.dismiss();
                                 finish();
-                                startActivity(new Intent(getApplicationContext() , Login.class));
-                            }catch (SQLException e)
-                            {
+                                startActivity(new Intent(getApplicationContext(), Login.class));
+                            } catch (SQLException e) {
                                 e.printStackTrace();
                             }
                         }
                     });
+                } else if (item.getTitle().toString().trim().equals("About Us")) {
+                    String url = "https://kms-technology.com/danang";
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(url));
+                    startActivity(i);
                 }
                 return true;
             }
